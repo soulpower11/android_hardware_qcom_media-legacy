e032eff4421beb7ea6784a13d392506e9229b234
lozohcum <lozohcum@gmail.com>
2014-05-15 22:07:18 +0200

    source upgrade

--- Android.mk
+++ Android.mk
@@ -3,8 +3,6 @@ QCOM_MEDIA_ROOT := $(call my-dir)
 ifneq ($(filter msm8974 msm8960 msm8660 msm7x27a msm7x27 msm7x30,$(TARGET_BOARD_PLATFORM)),)
 include $(QCOM_MEDIA_ROOT)/mm-core/Android.mk
 include $(QCOM_MEDIA_ROOT)/libstagefrighthw/Android.mk
-include $(QCOM_MEDIA_ROOT)/dashplayer/Android.mk
-include $(QCOM_MEDIA_ROOT)/QCMediaPlayer/Android.mk
 endif
 
 ifneq ($(filter msm8974 msm8960 msm8660 msm7x30,$(TARGET_BOARD_PLATFORM)),)
